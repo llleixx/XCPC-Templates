30ae74e0ca8042a2ebf499aa1b8556a1e9a7abbb
lllei <llleigoing@outlook.com>
2024-09-23 16:53:56 +0800

    Fix action

---
+++ script.sh
@@ -0,0 +1,3 @@
+mkdir fonts
+curl -o ./fonts/SourceHanSansSC-Light.otf https://raw.githubusercontent.com/adobe-fonts/source-han-sans/release/OTF/SimplifiedChinese/SourceHanSansSC-Light.otf
+curl -o ./fonts/SourceHanSansSC-Medium.otf https://raw.githubusercontent.com/adobe-fonts/source-han-sans/release/OTF/SimplifiedChinese/SourceHanSansSC-Medium.otf
