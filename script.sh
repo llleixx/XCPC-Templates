b9d34857b635a32c58b5c4a2b74e9889f06f3c4a
lllei <llleigoing@outlook.com>
2024-09-30 14:58:55 +0800

    Update mono font and TOC style

--- script.sh
+++ script.sh
@@ -1,3 +1,3 @@
 mkdir fonts
-curl -o ./fonts/SourceHanSansSC-Light.otf https://raw.githubusercontent.com/adobe-fonts/source-han-sans/release/OTF/SimplifiedChinese/SourceHanSansSC-Light.otf
-curl -o ./fonts/SourceHanSansSC-Medium.otf https://raw.githubusercontent.com/adobe-fonts/source-han-sans/release/OTF/SimplifiedChinese/SourceHanSansSC-Medium.otf
+curl -o ./fonts/SourceHanSansHWSC-Regular.otf https://raw.githubusercontent.com/adobe-fonts/source-han-sans/release/OTF/SimplifiedChineseHW/SourceHanSansHWSC-Regular.otf
+curl -o ./fonts/SourceHanSansHWSC-Bold.otf https://raw.githubusercontent.com/adobe-fonts/source-han-sans/release/OTF/SimplifiedChineseHW/SourceHanSansHWSC-Bold.otf
